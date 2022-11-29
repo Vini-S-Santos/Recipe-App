fd63c00a6aa6de5067e735b189ad88791f11d990
Vinicius <visouza0@gmail.com>
2022-11-29 18:04:24 -0300

    Adiciona Page Login

--- src/context/Provider.jsx
+++ src/context/Provider.jsx
@@ -1,10 +1,18 @@
-import React, { } from 'react';
+import React, { useState, useMemo } from 'react';
 import Context from './Context';
 
 function Provider(e) {
+  const [isValid, setIsValid] = useState(false);
+  const [user, setUser] = useState({
+    email: '',
+    password: '',
+  });
+
+  const values = useMemo(() => ({ isValid, setIsValid, user, setUser }), [isValid, user]);
+
   const { children } = e;
   return (
-    <Context.Provider>
+    <Context.Provider value={ values }>
       {children}
     </Context.Provider>
   );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,36 +1,74 @@
-import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import React, { useContext, useEffect } from 'react';
+import Context from '../context/Context';
 
-class Login extends Component {
-  render() {
-    return (
-      <>
-        <div>Login</div>
-        <label htmlFor="input-mail">
-          <input
-            type="text"
-            id="email-input"
-            data-testid="email-input"
-            name="email"
-          />
-        </label>
-        <label htmlFor="input-password">
-          <input
-            type="password"
-            id="password-input"
-            data-testid="password-input"
-            name="password"
-          />
-        </label>
-        <button
-          type="button"
-          data-testid="login-submit-btn"
-        >
-          Entrar
-        </button>
+function Login(props) {
+  const { isValid, setIsValid, user, setUser } = useContext(Context);
 
-      </>
-    );
-  }
+  const onChange = ({ target: { value, name } }) => {
+    setUser((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleClick = () => {
+    const { history } = props;
+    const { email } = user;
+    localStorage.setItem('user', JSON.stringify({ email }));
+    history.push('/meals');
+  };
+
+  useEffect(() => {
+    const magicNumber = 6;
+    const emailRegex = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/;
+    const checkMail = emailRegex.test(user.email);
+    const checkPassword = user.password.length > magicNumber;
+    if (checkMail && checkPassword) { setIsValid(false); } else { setIsValid(true); }
+  }, [user.email, user.password]);
+
+  return (
+    <div>
+      <div>Login</div>
+      <label htmlFor="input-mail">
+        <input
+          type="text"
+          id="email-input"
+          data-testid="email-input"
+          name="email"
+          value={ user.email }
+          onChange={ onChange }
+          placeholder="Email"
+        />
+      </label>
+      <label htmlFor="input-password">
+        <input
+          type="password"
+          id="password-input"
+          data-testid="password-input"
+          name="password"
+          value={ user.password }
+          onChange={ onChange }
+          placeholder="Password"
+        />
+      </label>
+      <button
+        type="button"
+        data-testid="login-submit-btn"
+        disabled={ isValid }
+        onClick={ handleClick }
+      >
+        Entrar
+      </button>
+
+    </div>
+  );
 }
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+}.isRequired;
 
 export default Login;
