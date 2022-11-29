8b8978c583293107dd65ca4cf230f4b9cb6bb24b
Joao Victor <imashinzo10@gmail.com>
2022-11-29 17:02:29 -0300

    Login arquivos

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function Login() {
-  return (
-    <h1>Login</h1>
-  );
+class Login extends Component {
+  render() {
+    return (
+      <>
+        <div>Login</div>
+        <label htmlFor="input-mail">
+          <input
+            type="text"
+            id="email-input"
+            data-testid="email-input"
+            name="email"
+          />
+        </label>
+        <label htmlFor="input-password">
+          <input
+            type="password"
+            id="password-input"
+            data-testid="password-input"
+            name="password"
+          />
+        </label>
+        <button
+          type="button"
+          data-testid="login-submit-btn"
+        >
+          Entrar
+        </button>
+
+      </>
+    );
+  }
 }
 
 export default Login;
