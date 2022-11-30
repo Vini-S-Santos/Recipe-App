d76ff5228659681b187ba5222300c31ceea84a18
Vinicius <visouza0@gmail.com>
2022-11-30 18:41:20 -0300

    adiciona requisito 25

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import FavoriteRecipes from './pages/FavoriteRecipes';
 import Login from './pages/Login';
 import Profile from './components/Profile';
 import Recipes from './pages/Recipes';
+import RecipeDetails from './pages/RecipeDetails';
 
 function App() {
   return (
@@ -12,8 +13,18 @@ function App() {
       <Route path="/done-recipes" component={ DoneRecipes } />
       <Route path="/favorite-recipes" component={ FavoriteRecipes } />
       <Route exact path="/" component={ Login } />
-      <Route path="/drinks" component={ Recipes } />
-      <Route path="/meals" component={ Recipes } />
+      <Route exact path="/drinks" component={ Recipes } />
+      <Route exact path="/meals" component={ Recipes } />
+      <Route
+        exact
+        path="/meals/:id"
+        render={ (props) => <RecipeDetails { ...props } /> }
+      />
+      <Route
+        exact
+        path="/drinks/:id"
+        render={ (props) => <RecipeDetails { ...props } /> }
+      />
       <Route path="/profile" component={ Profile } />
     </Switch>
   );
---
+++ src/components/Youtube.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Youtube({ embedId }) {
+  return (
+    <div className="video-responsive">
+      <iframe
+        data-testid="video"
+        width="853"
+        height="480"
+        src={ `https://www.youtube.com/embed/${embedId}` }
+        frameBorder="0"
+        allow="accelerometer; autoplay;
+       clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+        title="Embedded youtube"
+      />
+    </div>
+  );
+}
+
+Youtube.propTypes = { embedId: PropTypes.string.isRequired };
+export default Youtube;
